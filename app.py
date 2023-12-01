4fdbbe91d1d7cffc21b4b97d298ccafe472d5813
Rifat77-rma <152097495+Rifat77-rma@users.noreply.github.com>
2023-12-01 22:01:57 +0000

    register added

--- app.py
+++ app.py
@@ -3,10 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-  return render_template('home.html')
+@app.route('/')
+def home():
+  return render_template('index.html')
 
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+
+@app.route('/ratings')
+def ratings():
+  return render_template('ratings.html')
+
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
