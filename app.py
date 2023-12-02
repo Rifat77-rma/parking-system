f509d5d2b67e61cdee6c171d7624506f11cb2962
Rifat77-rma <152097495+Rifat77-rma@users.noreply.github.com>
2023-12-02 00:15:54 +0000

    Registration completed

--- app.py
+++ app.py
@@ -28,5 +28,16 @@ def register():
   return render_template('register.html')
 
 
+def Logging_In():
+  return render_template('index.html')
+
+
+def Cancel_Registration():
+  return render_template('index.html')
+
+
+
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
